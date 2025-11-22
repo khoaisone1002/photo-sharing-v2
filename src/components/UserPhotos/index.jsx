839da06c47b3ee59c8f5653ac24c80a0a6feb93b
khoaisone1002 <duong0985484554@gmail.com>
2025-11-22 10:34:54 +0700

    Fix comment empty message in UserPhotos

--- src/components/UserPhotos/index.jsx
+++ src/components/UserPhotos/index.jsx
@@ -18,30 +18,49 @@ function UserPhotos() {
     <Box p={2}>
       {photos.map((photo) => (
         <Box key={photo._id} mb={4}>
+          {/* HIỂN THỊ ẢNH */}
           <img
             src={`/images/${photo.file_name}`}
             alt={photo.file_name}
             className="user-photo"
           />
 
+          {/* NGÀY */}
           <Typography variant="caption" display="block">
             {new Date(photo.date_time).toLocaleString()}
           </Typography>
 
+          {/* COMMENT */}
           <Box mt={1}>
-            {photo.comments.map((c) => (
-              <Box key={c._id} ml={2} mb={1}>
-                <Typography variant="body2">
-                  <Link to={`/users/${c.user._id}`}>
-                    {c.user.first_name} {c.user.last_name}
-                  </Link>
-                  : {c.comment}
-                </Typography>
-                <Typography variant="caption">
-                  {new Date(c.date_time).toLocaleString()}
-                </Typography>
-              </Box>
-            ))}
+            {photo.comments.length === 0 ? (
+              <Typography
+                variant="body2"
+                ml={2}
+                color="text.secondary"
+                style={{ fontStyle: "italic" }}
+              >
+                (Không có comment nào.)
+              </Typography>
+            ) : (
+              photo.comments.map((c) => (
+                <Box key={c._id} ml={2} mb={1}>
+                  <Typography variant="body2">
+                    {c.user ? (
+                      <Link to={`/users/${c.user._id}`}>
+                        {c.user.first_name} {c.user.last_name}
+                      </Link>
+                    ) : (
+                      <span style={{ color: "gray" }}>Unknown user</span>
+                    )}
+                    : {c.comment}
+                  </Typography>
+
+                  <Typography variant="caption">
+                    {new Date(c.date_time).toLocaleString()}
+                  </Typography>
+                </Box>
+              ))
+            )}
           </Box>
         </Box>
       ))}
